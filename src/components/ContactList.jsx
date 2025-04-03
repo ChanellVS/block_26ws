f8865c0335fa96610d4c45d7915919342f1366c7
ChanellVS <zariachanellb@gmail.com>
2025-04-03 00:21:05 -0400

    Continue

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -11,7 +11,7 @@
   ];
 
 
- export default function ContactList(){
+ export default function ContactList({setSelectedContactId}){
 
 const [contacts, setContacts] = useState([]);
 
@@ -47,7 +47,7 @@ console.log(contacts)
             <td>Phone</td>
             </tr>
             {contacts.map((contact)=> {
-                return <ContactRow key={contact.id} contact={contact} />
+                return <ContactRow key={contact.id} contact={contact} setSelectedContactId={setSelectedContactId}/>
             })}
     </tbody>
 </table>
